c005029693e25e1f418f95ee7479f7fa90cc374a
KateKoss <kos.ka@outlook.com>
2017-05-14 15:04:57 +0300

    The "Мy аrticles" button is redefined.

---
+++ MvcApplication1/Controllers/ArticleController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplication1.Models;
+using System.Web.Security;
+using DotNetOpenAuth.AspNet;
+using Microsoft.Web.WebPages.OAuth;
+using WebMatrix.WebData;
+using MvcApplication1.Filters;
+using System.Text;
+using System.IO;
+
+namespace MvcApplication1.Controllers
+{
+    public class ArticleController : Controller
+    {
+
+
+    }
+}
