ef82c4bf7fcd8dc14caf4f8bf3ed725a3e24bf77
KateKoss <kos.ka@outlook.com>
2017-05-21 13:52:40 +0300

    Start working with student requests. Added RequestsModel and RequestsController.

---
+++ MvcApplication1/Controllers/RequestsController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplication1.Models;
+using System.Web.Security;
+using DotNetOpenAuth.AspNet;
+using Microsoft.Web.WebPages.OAuth;
+using WebMatrix.WebData;
+using MvcApplication1.Filters;
+using System.Text;
+using System.IO;
+using MvcApplication1.Contexts;
+using System.Reflection;
+
+namespace MvcApplication1.Controllers
+{
+    public class RequestsController : Controller
+    {
+        [HttpGet]
+        public ActionResult LoadStudentRequests()
+        {
+            RequestsModel requestsModel = new RequestsModel();
+            RequestsModel.Request req = new RequestsModel.Request("Help with Java", "bla-bla1", null, "request not resolved");
+            if (req.requestState != "request resolved" && req.requestState != "request canceled") requestsModel.reqests.Add(req);
+            req = new RequestsModel.Request("Help with C#", "bla-bla2", null, "request not resolved");
+            if (req.requestState != "request resolved" && req.requestState != "request canceled") requestsModel.reqests.Add(req);
+            return View("StudentRequests", requestsModel.reqests);
+
+        }
+        public ActionResult ActiveRequests()
+        {
+            return View();
+        }
+        public ActionResult ResolvedRequests()
+        {
+            return View();
+        }
+        public ActionResult CanceledRequests()
+        {
+            return View();
+        }
+    }
+}
---
+++ MvcApplication1/Models/RequestsModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Globalization;
+using System.Web.Mvc;
+using System.Web.Security;
+using System.Collections;
+
+namespace MvcApplication1.Models
+{
+    public class RequestsModel
+    {
+        public class Request
+        {
+            public Request(string requestName, string requestText, IEnumerable<SelectListItem> TegList, string requestState)
+            {
+                this.requestName = requestName;
+                this.requestText = requestText;
+                this.TegList = TegList;
+                this.requestState = requestState;
+            }
+            public string requestName { get; set; }
+            public string requestText { get; set; }
+            public IEnumerable<string> SelectedTeg { get; set; }
+            public IEnumerable<SelectListItem> TegList { get; set; }
+            public string requestState { get; set; } //Есть 3 состояния "request resolved", "request not resolved", "request canceled"
+        }
+
+        public List<Request> reqests = new List<Request>();
+    }
+}
