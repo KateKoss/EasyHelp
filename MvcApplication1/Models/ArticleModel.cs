ef1a5b29bb79d3ea646da19bc075f22257a23f0c
LudaZin <lu.zin.kpi@gmail.com>
2017-05-23 22:13:09 +0300

    add cs-files

---
+++ MvcApplication1/Models/ArticleModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Models
+{
+    [Table("Article")]
+    public class ArticleModel
+    {        
+        [Key]
+        public string articleID { get; set; }
+        public string createdBy { get; set; }
+        public IEnumerable<string> articleName { get; set; }
+        public IEnumerable<SelectListItem> articleNames { get; set; }
+        [Required(AllowEmptyStrings=false,ErrorMessage = "Введіть назву саттті.")]
+        public string articleTitle { get; set; }
+        public string articleText { get; set; }
+        public bool isPublished { get; set; }
+        
+    }
+}
