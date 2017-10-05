0a43738f9d2920d5098e0e414964859d2c5c26ca
LudaZin <lu.zin.kpi@gmail.com>
2017-10-05 21:57:05 +0300

    UserToken class for json obj

---
+++ MvcApplication1/UserToken.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MvcApplication1
+{
+    class UserToken
+    {
+        public string userName { get; set; }
+        public string access_token { get; set; }
+        public string token_type { get; set; }
+        public string expires_in { get; set; }
+        public string issued { get; set; }
+        public string expires { get; set; }
+    }
+}
