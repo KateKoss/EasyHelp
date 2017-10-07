fdd1d0549afe156c6fa8c24ceac940b3dadb219d
LudaZin <lu.zin.kpi@gmail.com>
2017-10-07 22:53:40 +0300

    add chat button to mentor preview list

---
+++ MvcApplication1/Models/ChatMassegeModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Models
+{
+    [Table("ChatMasseges")]
+    public class ChatMassegeModel
+    {
+        [Key]        
+        public int Id { get; set; }
+        public string FromUser { get; set; }
+        public string ToUser { get; set; }
+        public string Massege { get; set; }
+        public DateTime DateTimeSent { get; set; }
+    }
+}
